440eb22d0cf57ae25814e736fc75cf635f458e84
Lucas Galli <luquigalli0207@gmail.com>
2026-04-14 12:29:53 -0300

    add operators

--- Clase/Vector2.cpp
+++ Clase/Vector2.cpp
@@ -10,6 +10,20 @@ Vector3 Vector3::operator - (Vector3 other)
 	return (Vector3(this->x - other.x, this->y - other.y , this->z - other.z));
 }
 
+void Vector3::operator += (Vector3 other)
+{
+	this->x += other.x;
+	this->y += other.y;
+	this->z += other.z;
+}
+
+void Vector3::operator -= (Vector3 other)
+{
+	this->x -= other.x;
+	this->y -= other.y;
+	this->z -= other.z;
+}
+
 Vector3 Vector3::operator * (float scalar)
 {
 	return (Vector3(this->x * scalar, this->y * scalar, this->z * scalar));
@@ -18,6 +32,25 @@ Vector3 Vector3::operator * (float scalar)
 Vector3 Vector3::operator / (float scalar)
 {
 	return (Vector3(this->x / scalar, this->y / scalar, this->z / scalar));
+}
+
+void Vector3::operator *= (float scalar)
+{
+	this->x *= scalar;
+	this->y *= scalar;
+	this->z *= scalar;
+}
+
+void Vector3::operator /= (float scalar)
+{
+	this->x /= scalar;
+	this->y /= scalar;
+	this->z /= scalar;
+}
+
+float Vector3::operator ^ (Vector3 other)
+{
+	return ((this->y * other.z - this->x * other.y) - (this->x * other.z - this->z * other.x) + (this->x * other.y - this->y * other.x));
 }
 
 float Vector3::operator * (Vector3 other)
--- Clase/Vector2.h
+++ Clase/Vector2.h
@@ -12,7 +12,14 @@ struct Vector3
 
 	Vector3 operator + (Vector3 other);
 	Vector3 operator - (Vector3 other);
+	void operator += (Vector3 other);
+	void operator -= (Vector3 other);
 	Vector3 operator * (float scalar);
 	Vector3 operator / (float scalar);
+	void operator *= (float scalar);
+	void operator /= (float scalar);
+	Vector3 operator == (Vector3 other);
+	Vector3 operator != (Vector3 other);
 	float operator * (Vector3 other);
+	float operator ^ (Vector3 other);
 };
